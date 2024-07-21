f18d95b2a8ae79fd3144e93d7b5c13dab8429922
angel <angel.acedo2003@gmail.com>
2024-07-21 21:14:14 +0200

    change cors in vercel.json

--- frontend/src/configs/server.ts
+++ frontend/src/configs/server.ts
@@ -1,6 +1,6 @@
 const config = {
     clientUrl: "http://localhost:3001/",
-    serverUrl: "https://url-shortener-2xt3cidsb-angelacedos-projects.vercel.app/api/"
+    serverUrl: "https://url-shortener-phi-three.vercel.app/api/"
 
 }
 
